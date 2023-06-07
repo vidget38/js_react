61bf77a2722dde872bd3cd56adb85f25038e14a2
vidget38 <vidget38@mail.ru>
2023-06-07 12:55:48 +0300

    complite 2 task

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "")
+numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "")
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -8,12 +8,27 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-      b = +prompt("На сколько оцените его?", ""),
-      c = prompt("Один из последних просмотренных фильмов?", ""),
-      d = +prompt("На сколько оцените его?", "");
+for(i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+    b = +prompt("На сколько оцените его?", "");
+    if(a != null && b != null && a != "" && b != "" && a.length < 50 && b.length < 50){
+        personalMovieDB.movies[a] = b;
+    }else {
+        i--;
+    }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if(personalMovieDB.count < 10){
+    alert("Просмотрено довольно мало фильмов");
+}else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    alert("Вы классический зритель");
+}else if(personalMovieDB.count >= 30){
+    alert("Вы киноман");
+}else{
+    alert("Произошла ошибка");
+}
+
+
+
 
 console.log(personalMovieDB);
